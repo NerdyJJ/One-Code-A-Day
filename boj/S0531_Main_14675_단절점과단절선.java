2adfc7bd2f9f36d83aaf642d33dfe4457d0734a3
NerdyJJ <94119354+NerdyJJ@users.noreply.github.com>
2022-05-31 22:38:37 +0900

    rename

--- boj/S0531_Main_14675_단절점과단절선.java
+++ boj/S0531_Main_14675_단절점과단절선.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main_14675_단절점과단절선 {
+public class S0531_Main_14675_단절점과단절선 {
 
 	// https://www.acmicpc.net/problem/14675
 
